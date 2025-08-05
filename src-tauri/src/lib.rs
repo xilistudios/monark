962bb84370733f9063307582075e1d2d5611fa47
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2025-08-04 20:31:29 -0600

    feat: update dependencies and add fs plugin for file system access

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,7 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             vault::lifecycle::write_vault,
